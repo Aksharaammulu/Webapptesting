81faab096836f11f9c1b467d3701dcbc0c6225a3
Anji Reddy <106953908+Aksharaammulu@users.noreply.github.com>
2025-06-02 12:47:08 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    public class Class1
+    {
+
+        public Class1()
+        {
+
+        }
+
+        public void method1()
+        {
+            Console.WriteLine("Method 1");
+            Thread.Sleep(2000);
+        }
+
+        public async void method2()
+        {
+            Task.Delay(1000);
+            Console.WriteLine("Method 2");
+        }
+
+        public async void method3()
+        {
+            Task.Delay(1000);
+            Console.WriteLine("Method 3");
+        }
+
+    }
+}
